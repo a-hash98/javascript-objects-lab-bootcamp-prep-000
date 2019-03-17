74cc0459d8b6e4b726364ab29405c3938fb34077
Ay Has <ahashim983@yahoo.com>
2019-03-17 17:36:38 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ function deleteFromObjectByKey(object, key){
 
 
 function destructivelyDeleteFromObjectByKey(object, key){
-  return delete object.key
+  return delete Object.assign(object).key
 
 
 }
